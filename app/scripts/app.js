54333ca049fd8b4565f77811a1d4322d65fd2da5
Steve Jordan <sjordan@ntlworld.com>
2012-12-19 20:15:02 +0000

    comment heading changes for newer datepicker

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,39 +31,12 @@ require(
     ['namespace', 'collections/sermons', 'testdata', 'bootstrapdatepicker'],
     function (ns, Sermons, testData) {
 
-    //set some datepicker defaults
-    $.extend($.fn.datepicker.defaults, {
-
-        parse: function (string) {
-            var matches;
-            if ((matches = string.match(/^(\d{2,2})\/(\d{2,2})\/(\d{4,4})$/))) {
-                return new Date(matches[3], matches[1] - 1, matches[2]);
-            } else {
-                return null;
-            }
-        },
-
-        format: function (date) {
-            var
-            month = (date.getMonth() + 1).toString(),
-            dom = date.getDate().toString();
-            if (month.length === 1) {
-                month = '0' + month;
-            }
-            if (dom.length === 1) {
-                dom = '0' + dom;
-            }
-            return month + '/' + dom + '/' + date.getFullYear();
-        }
-
-    });
-
     //check for test data
-    // if (testData) {
-    //     Sermons.reset(Sermons.parse(testData));
-    // } else {
-    Sermons.fetch();
-    //}
+    if (testData) {
+        Sermons.reset(Sermons.parse(testData));
+    } else {
+        Sermons.fetch();
+    }
 
     var defaultSearchText = null;
 
@@ -107,12 +80,11 @@ require(
         $(e.target).val(defaultSearchText);
     })
 
-    .on('click change', '[name^="facet"]', function () {
+    .on('click', '[name^="facet"]:not([name*=date])', function () {
 
         //find out what has changed
         var speakersChosen = $('[name="facet[speaker]"]:checked').toArray(),
-            biblebooksChosen = $('[name^="facet[biblebook"]:checked').toArray(),
-            dateRangeChosen;
+            biblebooksChosen = $('[name^="facet[biblebook"]:checked').toArray();
 
         if (speakersChosen.length) {
             ns.Sermons.searchSpeakers(speakersChosen);
@@ -124,15 +96,19 @@ require(
             return;
         }
 
+        //ns.Sermons.resetList();
+
+    })
+
+    .on('dateready', function () {
+
         // + converts to unix timestamp
-        dateRangeChosen = {
+        var dateRangeChosen = {
             start: +new Date($('[name="facet[date-start]"]').val()) / 1000,
             end: +new Date($('[name="facet[date-end]"]').val()) / 1000
         };
 
         ns.Sermons.searchDate(dateRangeChosen);
-
-        //ns.Sermons.resetList();
 
     });
 
